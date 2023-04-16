4a97d2d2d556731d1ba152bbe1ecc9c17c848deb
Balazs527 <strauszbalazs2001@gmail.com>
2023-04-16 04:27:41 +0200

    ...........

--- users.sql
+++ users.sql
@@ -8,5 +8,10 @@ CREATE TABLE 'users' (
   'email' varchar(100) NOT NULL,
   'password' varchar(255) NOT NULL,
   'website' varchar(255),
+  role ENUM('user', 'admin') DEFAULT 'user' NOT NULL
   PRIMARY KEY ('id')
 );
+
+INSERT INTO users (username, password, email, role)
+VALUES ('user1', 'user1@example.com', 'password1', 'user'),
+       ('admin1', 'admin1@example.com', 'adminpassword1', 'admin');
