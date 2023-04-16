c7831578c6466442f8469f3c03630b924907b8d8
Strausz Balázs <127610608+Balazs527@users.noreply.github.com>
2023-04-16 04:49:38 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,17 +1,18 @@
 CREATE DATABASE db;
 USE db;
 
-CREATE TABLE 'users' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'fullname' varchar(100) NOT NULL,
-  'username' varchar(100) NOT NULL,
-  'email' varchar(100) NOT NULL,
-  'password' varchar(255) NOT NULL,
-  'website' varchar(255),
-  role ENUM('user', 'admin') DEFAULT 'user' NOT NULL
-  PRIMARY KEY ('id')
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  fullname varchar(100) NOT NULL,
+  username varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(255) NOT NULL,
+  website varchar(255),
+  role ENUM('user', 'admin') DEFAULT 'user' NOT NULL,
+  PRIMARY KEY (id)
 );
 
 INSERT INTO users (fullname, username, email, password, website, role)
-VALUES ('user11','user11', 'user1@example.com', 'password1','google.com', 'user'),
-       ('admin11','admin1', 'admin1@example.com', 'adminpassword1', 'google.com', 'admin');
+VALUES ('user11', 'user11', 'user1@example.com', 'password1', 'google.com', 'user'),
+       ('admin11', 'admin1', 'admin1@example.com', 'adminpassword1', 'google.com', 'admin');
+
